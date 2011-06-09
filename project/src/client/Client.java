7d9519d3bea0791d876cb75154dea6ebb32e478b
carl <carl.schoenbach@googlemail.com>
2011-06-09 22:37:27 +0200

    removed not used files from tutorial

--- project/src/client/Client.java
+++ project/src/client/Client.java
@@ -1,9 +1,6 @@
 package client;
 
 
-import bean.LineItem;
-import bean.Order;
-import bean.ShoppingCart;
 import bean.IUserMgt;
 import bean.Calendar;
 
@@ -20,25 +17,5 @@ public class Client
         Calendar cal = uMgt.getUserCalendar("email");
         if (cal == null)
             System.out.println("this user doesn't have a calendar --> error");
-
-
-      ShoppingCart cart = (ShoppingCart) ctx.lookup("ShoppingCartBean/remote");
-
-      System.out.println("Buying 2 memory sticks");
-      cart.buy("Memory stick", 2, 500.00);
-      System.out.println("Buying a laptop");
-      cart.buy("Laptop", 1, 2000.00);
-
-      System.out.println("Print cart:");
-      Order order = cart.getOrder();
-      System.out.println("Total: $" + order.getTotal());
-      for (LineItem item : order.getLineItems())
-      {
-         System.out.println(item.getQuantity() + "     " + item.getProduct() + "     " + item.getSubtotal());
-      }
-
-      System.out.println("Checkout");
-      cart.checkout();
-
-   }
+    }
 }
